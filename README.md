9adbc1e67cbf172e356e1c8f1d960cd4ccd36753
Allen Ng <allen@allenng.com>
2026-01-10 16:06:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# KarmaTester
+A simple test rig
+
+Karma and Mocha testers for playing with ideas.
---
+++ test.js
@@ -0,0 +1,18 @@
+const Logger = require('log-ng');
+const path = require('path');
+
+Logger({logLevel: 'info', logFile: 'mochaTest.log'});
+const logger = new Logger(path.basename(__filename));
+
+const app = require('./testServer.js');
+
+exports.mochaHooks = {
+	beforeAll(done){
+		this.testServer = app.listen(3000, 'localhost', done);
+		logger.info('Test server started');
+	},
+	afterAll(done){
+		this.testServer.close(done);
+		logger.info('Test server closed');
+	}
+};
